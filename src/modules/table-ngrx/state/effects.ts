951ddf1843dcc4dc9894e6f44c02ef17bc7c4ed7
myownsumm <myownsumm@gmail.com>
2022-05-13 16:02:35 +0300

    Things cleaned

--- src/modules/table-ngrx/state/effects.ts
+++ src/modules/table-ngrx/state/effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { delay, map, switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { DataService } from '../../data.service';
 import { fetchInitialData, initialDataFetched } from './actions';
 
--- src/modules/typings.ts
+++ src/modules/typings.ts
@@ -1,3 +1,5 @@
+// Typings are here just for simplification. Do not keep all things in one file at root!
+
 export interface Item {
   name: string;
   value: string;
