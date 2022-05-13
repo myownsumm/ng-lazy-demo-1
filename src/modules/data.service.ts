0800177fced9add18cb1d5de4031b1e6a90b32ee
myownsumm <myownsumm@gmail.com>
2022-05-13 15:58:07 +0300

    Load data from file

--- src/modules/data.service.ts
+++ src/modules/data.service.ts
@@ -2,21 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { Item } from './typings';
-
-const MOCKED_ITEMS = [
-  {name: 'first', value: 'some first value'},
-  {name: 'second', value: 'some second value'},
-  {name: 'third', value: 'some third value'},
-  {name: 'fourth', value: 'some fourth value'},
-  {name: 'fifth', value: 'some fifth value'},
-  {name: 'sixth', value: 'some sixth value'},
-];
+import data from './data.json';
 
 // Just for testing purposes. Use separate specific services in real cases
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class DataService {
   fetchItems(): Observable<Item[]> {
-    return of(MOCKED_ITEMS).pipe(
+    return of(data).pipe(
       delay(2000)
     );
   }
